d369ed111e93e46b20d965011c9cba49460110a7
biran varon <biraniv@gmail.com>
2024-05-07 21:29:01 +0300

    updates

--- server/index.js
+++ server/index.js
@@ -2,24 +2,31 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const routers = require('./routes/index')
-
 require('dotenv').config();
 
-const port = process.env.PORT || 5001
-
+const port = process.env.PORT || 5000;
 const app = express();
+
+// Middleware
+app.use(express.json());
 app.use(cors());
-app.use(express.json({ limit: '500mb' }));
 
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+        console.error("Error connecting to MongoDB:", error);
+        process.exit(1); // Terminate the process if unable to connect to MongoDB
+    });
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connection.once('open', () => {
-    console.log("MongoDB database connection established successfully")
-})
-
+// Routes
 app.use('/api', routers);
 
+// Start the server
 app.listen(port, () => {
-    console.log(`Server is running on port: ${ port }`)
+    console.log(`Server is running on port ${ port }`);
 });
+
 
