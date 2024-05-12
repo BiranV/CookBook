d08ba7aaa1f2a3e0b979c157935fd7da70908980
biran varon <biraniv@gmail.com>
2024-05-13 01:55:35 +0300

    style PDF export file

--- client/src/components/RecipeExport.js
+++ client/src/components/RecipeExport.js
@@ -4,12 +4,26 @@ const RecipeExport = ({ recipe }) => {
 
     const handleExportPDF = () => {
         const doc = new jsPDF();
+        doc.setFont("helvetica", "bold");
+        doc.setFontSize(16);
+        const textWidth = doc.getStringUnitWidth(recipe.title) * 16 / doc.internal.scaleFactor;
         doc.text(recipe.title, 10, 10);
+        doc.setLineWidth(0.5);
+        doc.line(10, 12, 10 + textWidth, 12); // Drawing a line below the title to simulate underline
+        doc.setFont("helvetica", "normal");
+        doc.setFontSize(12);
+        doc.setFont("helvetica", "bold");
         doc.text("Ingredients:", 10, 25);
+        doc.setFont("helvetica", "normal");
+        doc.setFontSize(9);
         recipe.ingredients.forEach((ingredient, index) => {
             doc.text(`${ index + 1 }. ${ ingredient }`, 10, 35 + (index * 10));
         });
+        doc.setFontSize(12);
+        doc.setFont("helvetica", "bold");
         doc.text("Steps:", 10, 45 + (recipe.ingredients.length * 10));
+        doc.setFont("helvetica", "normal");
+        doc.setFontSize(9);
         recipe.steps.forEach((step, index) => {
             doc.text(`${ index + 1 }. ${ step }`, 10, 55 + (recipe.ingredients.length * 10) + (index * 10));
         });
