53e1ca4352b02e495e027c9f241483c0a4d98bcd
BiranV <59264488+BiranV@users.noreply.github.com>
2024-07-18 02:46:50 +0000

    update

--- client/src/components/MessageForm.js
+++ client/src/components/MessageForm.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import axios from '../api/axios';
 
-const MessageForm = ({ recipent }) => {
+const MessageForm = ({ recipient  }) => {
     const [name, setName] = useState('');
     const [message, setMessage] = useState('');
     const [sending, setSending] = useState(false);
@@ -13,7 +13,7 @@ const MessageForm = ({ recipent }) => {
         setSending(true);
         try {
             const token = localStorage.getItem("token");
-            const response = await axios.post('/messages', { recipent, name, message }, {
+            const response = await axios.post('/messages', { recipient , name, message }, {
                 headers: {
                     Authorization: `Bearer ${token}`
                 }
--- server/models/auth.js
+++ server/models/auth.js
@@ -9,23 +9,7 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
-    },
-    messages: [
-        {
-            from: {
-                type: String,
-                required: true
-            },
-            message: {
-                type: String,
-                required: true
-            },
-            createdAt: {
-                type: Date,
-                default: Date.now
-            }
-        }
-    ]
+    }
 });
 
 module.exports = mongoose.model('User', userSchema);
